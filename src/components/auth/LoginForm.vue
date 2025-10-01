db25932d7495a11fdaf2eec2b185d1733776d801
Treat William <treatwilliam.almonia1@gmail.com>
2025-10-01 13:21:01 +0800

    feat: added registration function

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -43,30 +43,30 @@ const onFormSubmit = () => {
     <!-- Password -->
     <v-text-field
       v-model="formData.password"
-      :type="showPassword ? 'text' : 'password'"
+      :type="isPasswordVisible ? 'text' : 'password'"
       label="Password"
       placeholder="Enter Your Password"
       outlined
       dense
       class="mb-2"
-      :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
-      @click:append-inner="showPassword = !showPassword"
+      :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+      @click:append-inner="isPasswordVisible = !isPasswordVisible"
       :rules="[requiredValidator]"
     />
 
     <!-- Remember Me + Forgot Password -->
-    <div class="d-flex justify-space-between align-center mb-4 text-small">
+    <!-- <div class="d-flex justify-space-between align-center mb-4 text-small">
       <v-checkbox v-model="rememberMe" hide-details density="compact" class="text-small">
         <template #label>
           <span class="text-small">Remember Me</span>
         </template>
       </v-checkbox>
       <a href="#" class="text-red text-decoration-none">Forgot Password?</a>
-    </div>
+    </div> -->
 
     <!-- Login Button -->
-    <v-btn block color="primary" class="mb-4" height="45" @click="login">
-      <RouterLink to="/dashboard">Login</RouterLink>
+    <v-btn block color="primary" class="mb-4" height="45" type="submit">
+      <!-- <RouterLink to="/dashboard">Login</RouterLink> -->Login
     </v-btn>
 
     <!-- Divider with text -->
@@ -104,7 +104,7 @@ const onFormSubmit = () => {
       <RouterLink :to="{ name: 'register' }" class="text-blue text-decoration-none">
         Sign Up
       </RouterLink>
-      >
+      
     </div>
   </v-form>
 </template>
